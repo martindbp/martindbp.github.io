147d4b9eb3ab61159fabfe19f6c03b193de880c7
Martin Pettersson <marpett@kth.se>
2019-09-18 18:49:04 +0100

    Nikola auto commit.
    Source commit: 18424546c1b9dd99d32277eaf625760aa2a16316
    Nikola version: 8.0.2

--- themes/bootstrap3-gradients/toggle.js
+++
@@ -1,9 +0,0 @@
-function code_toggle() {
-    if (!code_show){
-      $('div.input').hide();
-    //$('div.prompt.output_prompt').hide();  //unnecessary since I'm hiding all prompts anyways in css
-    } else {
-      $('div.input').show();
-    }
-   code_show = !code_show
-}
